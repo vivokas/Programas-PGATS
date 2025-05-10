aa201bffc17d7cd40ac50d0d43fe1cb42f35407e
Vivokas <vivianevinici@gmail.com>
2025-05-10 12:06:51 -0300

    a10

---
+++ Programas-PGATS/Projeto_2/Conceitos JS/repeticao-for.js
@@ -0,0 +1,11 @@
+/*
+* para, faça 
+* 
+
+*/
+
+const quantidadePetisco = 3
+
+for(let quantidadePetisco = 1; quantidadePetisco <= quantidadeCalculada; quantidadePetisco++){
+    console.log(`Dando petisco de n° ${quantidadePetisco}´) 
+}
---
+++ Programas-PGATS/Projeto_2/Conceitos JS/repeticao-while.js
@@ -0,0 +1,8 @@
+
+let voltasAteFicarCansado = 5
+let quantidadeVoltasAtual = 0
+
+while (quantidadeVoltasAtual <= voltasAteFicarCansado){
+    console.log(`Dar uma volta na quadra´)
+}
+return
